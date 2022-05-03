f4df193f26f5aa723a80f94afe5f379bf50e1201
ren xiang <hello@lifeupsg.com>
2022-05-03 17:18:07 +0800

    hi

--- farmfresh_curl.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, 'https://sahithikhandavalli.com/top5.php');
-	curl_exec ($ch);
-	curl_close($ch);
-?>
--- paperco_curl.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, 'http://vandanachandola.online/top5.php');
-	curl_exec ($ch);
-	curl_close($ch);
-?>
